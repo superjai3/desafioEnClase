fbc9e9fd918a00428cf42e41e0f577e6c566f2e4
Jaime Valdés <96346901+superjai3@users.noreply.github.com>
2022-05-22 12:53:33 -0300

    2da alternativa

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -1,9 +1,24 @@
-numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
+// numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
 
-while (numero <= 1000) {
-  alert("Ingresaste " + numero);
-  if (numero < 500) {
-    alert("Estas poniendo menos de la mitad");
-  }
-  numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
+// while (numero <= 1000) {
+//   alert("Ingresaste " + numero);
+//   if (numero < 500) {
+//     alert("Estas poniendo menos de la mitad");
+//   }
+//   numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
+// }
+
+let numeroIngresado = parseInt(prompt("Ingrese por favor un Número: "));
+
+debugger;
+for (let i = 0; i < 11; i++) {
+  let resultado = numeroIngresado + i;
+  alert(
+    "A tu número ingresado le sumaremos de 1 en 1: " +
+      numeroIngresado +
+      "+" +
+      i +
+      " = " +
+      resultado
+  );
 }
