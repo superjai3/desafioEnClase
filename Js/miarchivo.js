60a26f22c88b503e75d842e261ad641c46659d50
Jaime Valdés <96346901+superjai3@users.noreply.github.com>
2022-06-04 18:25:54 -0300

    Mi primer git

--- Js/miarchivo.js
+++ Js/miarchivo.js
@@ -1,27 +1,42 @@
-//ALTERNETIVA 1
+//Cotizador de póliza de vida
 
-// numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
+let anioActual = 2022;
+let anioNacimiento = parseInt(prompt("Ingrege su año de nacimiento: "));
 
-// while (numero <= 1000) {
-//   alert("Ingresaste " + numero);
-//   if (numero < 500) {
-//     alert("Estas poniendo menos de la mitad");
-//   }
-//   numero = parseInt(prompt("Ingrese por favor un Número > que 1000: "));
-// }
+let edad = parseInt(anioActual - anioNacimiento);
 
-//ALTERNETIVA 2
-let numeroIngresado = parseInt(prompt("Ingrese por favor un Número: "));
-
-// debugger;
-for (let i = 1; i < 11; i++) {
-  let resultado = numeroIngresado + i;
-  alert(
-    "A tu número ingresado le sumaremos de 1 en 1: " +
-      numeroIngresado +
-      "+" +
-      i +
-      " = " +
-      resultado
-  );
+if (edad <= 0 || edad > 80) {
+  alert(`Su edad: ${edad}, se encuentra fuera de rango`);
+} else if (edad > 0 && edad < 80) {
+  if (edad < 18) {
+    alert(
+      `Usted es menor de edad, tiene ${edad} años, debe ser mayor de edad.`
+    );
+  } else if (edad >= 18 && edad <= 65) {
+    alert(`Usted es una persona apta para la póliza de vida`);
+    alert(`Su edad es: ${edad} años`);
+    let sueldoActual = parseInt(prompt("Ingrese su sueldo actual: "));
+    capital = sueldoActual * 24;
+    Tasa = 2;
+    let costo = (Tasa * sueldoActual) / 1000;
+    alert(
+      `El costo de la póliza de Vida es: $${costo} por mes y el capital asegurado sería de $${capital}`
+    );
+  } else if (edad >= 66 && edad <= 80) {
+    alert(
+      `Usted puede contratar la póliza de vida, bajo la opción Mayores de 65 años`
+    );
+    alert(`Su edad es: ${edad} años`);
+    let sueldoActual = parseInt(prompt("Ingrese su sueldo actual: "));
+    capital = sueldoActual * 24;
+    Tasa = 3;
+    let costo = (Tasa * sueldoActual) / 1000;
+    alert(
+      `El costo de la póliza de Vida es: $${costo} por mes y el capital asegurado sería de $${capital}`
+    );
+  } else if (edad >= 76) {
+    alert(
+      `Usted se encuentra fuera del rango de esta cobertura, tiene actualmente ${edad} años, gracias por su consulta`
+    );
+  }
 }
